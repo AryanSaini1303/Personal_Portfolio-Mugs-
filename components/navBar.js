21610a89694cc36f1198004ea1ffd0324078ded6
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-08-11 22:03:11 +0530

    Feat: Added the navbar

---
+++ components/navBar.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import styles from "./navBar.module.css"
+import { Montserrat } from "next/font/google";
+
+const mont=Montserrat({
+    weight:['400'],
+    subsets:['latin']
+})
+
+export default function NavBar() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.logoSection}>
+        <Image src={"/logos/name_logo.png"} width={50} height={50} />
+        <h1 className={`${styles.name} ${mont.style}`}>Mumair</h1>
+      </div>
+      <div className={styles.navs}>
+        <ul>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Projects</li>
+            <li>Testimonials</li>
+            <li>Contact</li>
+            <li>Download CV</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
